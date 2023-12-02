ae013d2be99592da4a7e4914c53f3647db5bb7f5
Chavan Pranit <chavapra@ap.dtcs.local>
2023-12-02 18:58:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="hero">
+        <div className="container">
+          <nav className="hero__nav">
+            <h1>adad</h1>
+            <ul className="hero__nav--link">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
